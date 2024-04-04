5a164013b5ac9b085aeb4edfdf792102817dfd94
olivebishop <olivehendrilgen1@gmail.com>
2024-04-04 18:39:36 +0300

    updates on products

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,28 +1,28 @@
-import { render } from '@react-email/components';
-import WelcomeEmail from '../emails/WelcomeEmail';
-import { Resend } from 'resend';
+// import { render } from '@react-email/components';
+// import WelcomeEmail from '../emails/WelcomeEmail';
+// import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(request) {
-  try {
-    const { email, userFirstname } = await request.json();
+// export async function POST(request) {
+//   try {
+//     const { email, userFirstname } = await request.json();
 
-    const html = render(<WelcomeEmail userFirstname={userFirstname} />);
+//     const html = render(< WelcomeEmail userFirstname={userFirstname} />);
 
-    const { data, error } = await resend.emails.send({
-      from: 'e-ticko <noreply@olivebishop.me>',
-      to: [email],
-      subject: 'Thank you for e-ticko events ticket purchase!',
-      html: html,
-    });
+//     const { data, error } = await resend.emails.send({
+//       from: 'e-ticko <noreply@olivebishop.me>',
+//       to: [email],
+//       subject: 'Thank you for e-ticko events ticket purchase!',
+//       html: html,
+//     });
 
-    if (error) {
-      return new Response(JSON.stringify({ error }), { status: 500 });
-    }
+//     if (error) {
+//       return new Response(JSON.stringify({ error }), { status: 500 });
+//     }
 
-    return new Response(JSON.stringify({ data }), { status: 200 });
-  } catch (error) {
-    return new Response(JSON.stringify({ error }), { status: 500 });
-  }
-}
+//     return new Response(JSON.stringify({ data }), { status: 200 });
+//   } catch (error) {
+//     return new Response(JSON.stringify({ error }), { status: 500 });
+//   }
+// }
