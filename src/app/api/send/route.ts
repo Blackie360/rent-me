669ce872873fc89e7563d8e38ae24185ff25e247
olivebishop <olivehendrilgen1@gmail.com>
2024-04-07 22:33:59 +0300

    fixes on emial route

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -4,17 +4,14 @@ import WelcomeEmailTemplate from '../../emails/WelcomeEmailTemplate';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-interface PostRequestBody {
-  recipientEmail: string;
-}
+ 
     
-export async function POST({ body }: { body: PostRequestBody }) {
+    export async function POST() {
       try {
-        const { recipientEmail } = body;
     // Send the email using Resend
     const data = await resend.emails.send({
       from: 'e-ticko <noreply@olivebishop.me>',
-      to: [recipientEmail],
+      to:  ['delivered@resend.dev'],
       reply_to: 'support@olivebishop.me',
       subject: 'Thank you for using e-ticko events ticket purchase!',
       react:WelcomeEmailTemplate({ userFirstname: "olive", ticketId: "123456" })
