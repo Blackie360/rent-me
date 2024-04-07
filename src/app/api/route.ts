1eb248e4b6274fb9d61586a916e2edb1e4e09546
olivebishop <olivehendrilgen1@gmail.com>
2024-04-07 22:28:01 +0300

    fixes

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,43 +1,20 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { Resend } from 'resend';
-import WelcomeEmailTemplate from '../../app/emails/WelcomeEmailTemplate';
-import { render } from '@react-email/components'; // Import the render function
+import { NextRequest, NextResponse } from "next/server";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+export async function GET(request: NextRequest) {
+  const requestUrl = request.url;
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  try {
-    if (req.method !== 'POST') {
-      return res.status(405).json({ error: 'Method Not Allowed' });
-    }
-
-    const { email } = req.body;
-    if (!email) {
-      return res.status(400).json({ error: 'Email is required' });
-    }
-
-    // Render the WelcomeEmail component
-    
-    const emailContent = render(WelcomeEmailTemplate({ userFirstname: "olive", ticketId: "123456" }));
-
-
-    
-    console.log('Sending email:', emailContent); // Log the email content for debugging
-
-    // Send the email using Resend
-    const response = await resend.emails.send({
-      from: 'e-ticko <noreply@olivebishop.me>',
-      to: [email],
-      reply_to: 'support@olivebishop.me',
-      subject: 'Thank you for using e-ticko events ticket purchase!',
-      react: emailContent,
-    });
-
-    console.log('Email response:', response); // Log the email send response for debugging
-
-    return res.status(200).json({ status: 200, message: 'Email sent successfully' });
-  } catch (error) {
-    console.error('Error sending email:', error);
-    return res.status(500).json({ error: 'Internal Server Error' });
-  }
+  return NextResponse.json({
+    message: "Hello from the API",
+    request: requestUrl,
+  });
 }
+
+export async function HEAD(request: NextRequest) {}
+
+export async function POST(request: NextRequest) {}
+
+export async function PUT(request: NextRequest) {}
+
+export async function DELETE(request: NextRequest) {}
+
+export async function PATCH(request: NextRequest) {}
---
+++ src/app/api/send/route.ts
@@ -0,0 +1,27 @@
+
+import { Resend } from 'resend';
+import WelcomeEmailTemplate from '../../emails/WelcomeEmailTemplate';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+interface PostRequestBody {
+  recipientEmail: string;
+}
+    
+export async function POST({ body }: { body: PostRequestBody }) {
+      try {
+        const { recipientEmail } = body;
+    // Send the email using Resend
+    const data = await resend.emails.send({
+      from: 'e-ticko <noreply@olivebishop.me>',
+      to: [recipientEmail],
+      reply_to: 'support@olivebishop.me',
+      subject: 'Thank you for using e-ticko events ticket purchase!',
+      react:WelcomeEmailTemplate({ userFirstname: "olive", ticketId: "123456" })
+    });
+
+    return Response.json(data);
+  } catch (error) {
+    return Response.json({ error });
+  }
+}
